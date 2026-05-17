e96c54f508ca1c4e405fde9ad543dc239e829070
Isabella Yogui <isabellayk13@gmail.com>
2026-05-17 20:20:11 -0300

    Feat: Criando validação para o forms

--- src/js/scriptContato.js
+++ src/js/scriptContato.js
@@ -13,3 +13,35 @@ window.onload = () => {
     }, 1000);
 
 }
+// Form
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", () => {
+
+    const nome = document.getElementById("nome").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
+
+    const notificacao = document.getElementById("notificacao");
+
+    if(nome === "" || email === "" || mensagem === ""){
+
+        notificacao.innerHTML = "Preencha todos os campos!";
+        notificacao.style.background = "#e53935";
+
+    }else{
+
+        notificacao.innerHTML = "Mensagem enviada com sucesso!";
+        notificacao.style.background = "#43a047";
+
+        document.getElementById("formulario").reset();
+
+    }
+
+    notificacao.classList.add("mostrar");
+
+    setTimeout(() => {
+        notificacao.classList.remove("mostrar");
+    }, 3000);
+
+});
